ac52a333709845aeb9fef61f888965f519785e99
ariel gribov <argr96@gmail.com>
2022-08-31 17:17:33 +0300

    forth commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const hamburgerButtonCheckbox = document.getElementById('checkbox');
 const navBar = document.getElementById('inner-nav-container');
-console.log(navBar);
 hamburgerButtonCheckbox.onclick = () => {
     navBar.classList.toggle('show-inner-nav-container');
+};
+const careers = document.getElementById('careers');
+const careersDropDown = document.getElementById('careers-dropdown');
+console.log(careersDropDown);
+careers.onclick = () => {
+    careers.classList.toggle('show-careers-dropdown');
 };

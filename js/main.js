316670f59092e47dff41a00d1e05ca27c50b1235
ariel gribov <argr96@gmail.com>
2022-09-04 17:15:52 +0300

    header done+start main

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 const hamburgerButtonCheckbox = document.getElementById('checkbox');
 const navBar = document.getElementById('inner-nav-container');
-hamburgerButtonCheckbox.onclick = () => {
-    navBar.classList.toggle('show-inner-nav-container');
-};
+const headerContainer = document.getElementById('header');
 const careers = document.getElementById('careers');
 const careersDropDown = document.getElementById('careers-dropdown');
 
-// console.log(window.innerWidth);
+hamburgerButtonCheckbox.onclick = () => {
+    navBar.classList.toggle('show-inner-nav-container');
+};
 careers.onclick = () => {
-    if (window.innerWidth < 925) careers.classList.toggle('show-careers-dropdown');
+    if (window.innerWidth < 925) {
+        careersDropDown.classList.toggle('show-careers-dropdown');
+    }
 };

127aaaafd2f208e296edf447e1ba47597f1bcba8
ariel gribov <argr96@gmail.com>
2022-09-11 17:51:37 +0300

    footer and more

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const navBar = document.getElementById('inner-nav-container');
 const headerContainer = document.getElementById('header');
 const careers = document.getElementById('careers');
 const careersDropDown = document.getElementById('careers-dropdown');
+const question = document.getElementsByClassName('question');
+const answer = document.getElementsByClassName('answer');
+const categories = document.getElementsByClassName('category-container');
+const catAns = document.getElementsByClassName('category-inner-menu');
+const pluses = document.getElementsByClassName('fa-thin');
 
 hamburgerButtonCheckbox.onclick = () => {
     navBar.classList.toggle('show-inner-nav-container');
@@ -12,4 +17,22 @@ careers.onclick = () => {
         careersDropDown.classList.toggle('show-careers-dropdown');
     }
 };
-window.addEventListener('resize', () => {});
+
+for (let i = 0; i < question.length; i++) {
+    question[i].onclick = () => {
+        answer[i].classList.toggle('show-answer');
+        if (question[i].lastElementChild.classList.contains('fa-arrow-circle-down')) {
+            question[i].lastElementChild.classList.remove('fa-arrow-circle-down');
+            question[i].lastElementChild.classList.add('fa-arrow-circle-up');
+        } else if (question[i].lastElementChild.classList.contains('fa-arrow-circle-up')) {
+            question[i].lastElementChild.classList.remove('fa-arrow-circle-up');
+            question[i].lastElementChild.classList.add('fa-arrow-circle-down');
+        }
+    };
+}
+for (let i = 0; i < categories.length; i++) {
+    categories[i].onclick = () => {
+        catAns[i].classList.toggle('show-category');
+        pluses[i].classList.toggle('rotate-to-x');
+    };
+}
